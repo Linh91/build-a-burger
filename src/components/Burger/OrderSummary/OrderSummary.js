c31e58b6d5ee0463a8423fe23df7fb697cc15f59
sammy.ha <sammy.ha@kingfisher.com>
2018-06-27 16:55:11 +0100

    added price on order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total price: {props.price}</strong></p>
             <p>Continue to checkout</p>
             <Button btnType="Danger" clicked={props.purcahseCanceled}>Cancel</Button>
             <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
